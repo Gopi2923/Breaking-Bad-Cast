b191e1aef808c4329ae1521b1e4bd847650907b2
Gopi2923 <dgopidhikshan@gmail.com>
2024-08-10 23:30:09 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Header from './Components/ui/Header'
+import './App.css'
+import './index.css'
+
 
 const App = () => {
+
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+     const fetchItems = async () => {
+      const result = await axios(` https://api.breakingbadquotes.xyz/v1/quotes`)
+
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+     }
+
+     fetchItems()
+  }, [])
   return (
     <div className='container'>
-       Hello
+     <Header />
     </div>
   )
 }
